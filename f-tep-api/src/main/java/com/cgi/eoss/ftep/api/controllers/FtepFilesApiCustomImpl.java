a49b535dfd5ee17dc26f0c594e911dd2460ba483
Lauri Seitsonen <lauri.seitsonen@vtt.fi>
2022-04-06 12:37:29 +0000

    Split ftepFiles/findAll to multiple queries to circumvent 32767 bind variables limit

--- f-tep-api/src/main/java/com/cgi/eoss/ftep/api/controllers/FtepFilesApiCustomImpl.java
+++ f-tep-api/src/main/java/com/cgi/eoss/ftep/api/controllers/FtepFilesApiCustomImpl.java
@@ -9,19 +9,25 @@ import com.cgi.eoss.ftep.model.User;
 import com.cgi.eoss.ftep.persistence.dao.FtepFileDao;
 import com.cgi.eoss.ftep.security.FtepSecurityService;
 import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.NumberPath;
 import com.querydsl.jpa.JPQLQuery;
 import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.querydsl.QPageRequest;
 import org.springframework.data.repository.support.PageableExecutionUtils;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
+@Log4j2
 @Getter
 @Component
 public class FtepFilesApiCustomImpl extends BaseRepositoryApiImpl<FtepFile> implements FtepFilesApiCustom {
@@ -93,8 +99,46 @@ public class FtepFilesApiCustomImpl extends BaseRepositoryApiImpl<FtepFile> impl
             return PageableExecutionUtils.getPage(query.fetch(), pageable, query::fetchCount);
         } else {
             Set<Long> visibleIds = getSecurityService().getVisibleObjectIds(getEntityClass(), getDao().findAllIds());
-            query.where(getIdPath().in(visibleIds));
-            return PageableExecutionUtils.getPage(query.fetch(), pageable, query::fetchCount);
+            // Partition the visible ids to subsets < 32767 elements as the DB 
+            // interface breaks if there are over 32767 bind variables in a query
+            List<Long> visibleIdsList = Lists.newArrayList(visibleIds);
+            List<List<Long>> partitionedIds = Lists.partition(visibleIdsList, 30000);
+
+            long offset = 0;
+            int filesRequested = 20;
+            if (pageable != null) {
+                offset = pageable.getOffset();
+                filesRequested = pageable.getPageSize();
+            }
+
+            // The collected result files, at most filesRequested files
+            List<FtepFile> files = new ArrayList<>();
+            // The total number of available files
+            long totalCount = 0;
+
+            // Run the query for all partitions
+            // Sum the number of items in each partition to get the total item count
+            //  and collect the requested number of files from the requested offset onwards
+            for (List<Long> ids : partitionedIds) {
+                JPQLQuery<FtepFile> q = from(QFtepFile.ftepFile).where(predicate);
+                q.where(getIdPath().in(ids));
+                
+                long fetchCount = q.fetchCount();
+
+                if (totalCount + fetchCount > offset && files.size() < filesRequested) {
+                    // Add files from the correct offset (i.e. skip the first offset files
+                    long start = offset - totalCount;
+                    if (start < 0) {
+                        start = 0;
+                    }
+                    q = q.offset(start).limit(filesRequested - files.size());
+                    files.addAll(q.fetch());
+                }
+                totalCount += fetchCount;
+            }
+            // Total result item count as a final long so that conversion to LongSupplier works
+            final long total = totalCount;
+            return PageableExecutionUtils.getPage(files, pageable, () -> new Long(total));
         }
     }
 
