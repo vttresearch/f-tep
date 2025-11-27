9d872f51d704b9abda8bf49920a37a27dfc39b45
Seitsonen Lauri <Lauri.Seitsonen@vtt.fi>
2025-11-27 15:33:21 +0200

    Fix

--- f-tep-persistence/src/main/java/com/cgi/eoss/ftep/persistence/service/JpaFtepTermsDataService.java
+++ f-tep-persistence/src/main/java/com/cgi/eoss/ftep/persistence/service/JpaFtepTermsDataService.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import static com.cgi.eoss.ftep.model.QFtepTerms.ftepterms;
+import static com.cgi.eoss.ftep.model.QFtepTerms.ftepTerms;
 
 @Service
 @Transactional(readOnly = true)
