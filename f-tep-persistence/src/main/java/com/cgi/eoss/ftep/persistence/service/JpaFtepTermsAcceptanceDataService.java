d7ad6bbadc55f575635eba745b6d5989a308490f
Seitsonen Lauri <Lauri.Seitsonen@vtt.fi>
2025-11-27 13:49:47 +0200

    Fix

--- f-tep-persistence/src/main/java/com/cgi/eoss/ftep/persistence/service/JpaFtepTermsAcceptanceDataService.java
+++ f-tep-persistence/src/main/java/com/cgi/eoss/ftep/persistence/service/JpaFtepTermsAcceptanceDataService.java
@@ -31,7 +31,7 @@ public class JpaFtepTermsAcceptanceDataService extends AbstractJpaDataService<Ft
 
     @Override
     Predicate getUniquePredicate(FtepTermsAcceptance entity) {
-        return ftepTermsAcceptance.user.eq(entity.getUser()).and(ftepTermsAcceptanceDao.acceptanceTime.eq(entity.getAcceptanceTime()));
+        return ftepTermsAcceptance.owner.eq(entity.getOwner()).and(ftepTermsAcceptance.acceptanceTime.eq(entity.getAcceptanceTime()));
     }
 
     @Override
