d094483edb57e2868955027c13b2c0176898a578
Seitsonen Lauri <Lauri.Seitsonen@vtt.fi>
2025-11-28 15:50:25 +0200

    Service terms fix

--- f-tep-model/src/main/java/com/cgi/eoss/ftep/model/FtepTerms.java
+++ f-tep-model/src/main/java/com/cgi/eoss/ftep/model/FtepTerms.java
@@ -10,6 +10,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.Optional;
